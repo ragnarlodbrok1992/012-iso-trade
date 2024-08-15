46cd96314537bac4e2f517fb3fa1b00946af1c16
ragnarlodbrok1992 <maraus92@gmail.com>
2024-08-15 19:44:22 +0200

    WIP

--- src/iso_trade.cpp
+++ src/iso_trade.cpp
@@ -17,6 +17,37 @@ static GLFWwindow* window = NULL;
  * Shaders code as static strings
  */
 
+const char* default_shader_vertex =
+R"(
+#version 330 core
+
+layout(location = 0) in vec3 vertexPosition_modelspace;
+layout(location = 1) in vec3 vertexColor;
+
+out vec3 fragmentColor;
+
+uniform mat4 MVP;
+
+void main() {
+  gl_Position = MVP * vec4(vertexPosition_modelspace, 1);
+
+  fragmentColor = vertexColor;
+}
+)";
+
+const char* default_shader_fragment =
+R"(
+#version 330 core
+
+in vec3 fragmentColor;
+
+out vec3 color;
+
+void main() {
+  color = fragmentColor;
+}
+)";
+
 /**
  * Shader loading function
  */
@@ -62,8 +93,20 @@ GLuint load_shaders(const char* vertex_source, const char* fragment_source) {
   glLinkProgram(program_id);
 
   // Check the program
+  glGetProgramiv(program_id, GL_LINK_STATUS, &result);
+  glGetProgramiv(program_id, GL_INFO_LOG_LENGTH, &info_log_length);
+  if (info_log_length > 0) {
+    GLchar program_error_message[512];
+    glGetProgramInfoLog(program_id, info_log_length, NULL, &program_error_message[0]);
+    printf("%s\n", &program_error_message[0]);
+  }
 
   // Cleanup - detach and delete shaders - they are no longer needed since they are now part of the program
+  glDetachShader(program_id, vertex_shader_id);
+  glDetachShader(program_id, fragment_shader_id);
+
+  glDeleteShader(vertex_shader_id);
+  glDeleteShader(fragment_shader_id);
   
   return program_id;
 }
@@ -97,9 +140,14 @@ int main(int argc, char** argv) {
   // Make the window's context current
   glfwMakeContextCurrent(window);
 
+  // glewExperimental
+  glewExperimental = true;
+
   // Initialize GLEW
   if (glewInit() != GLEW_OK) {
     printf("Failed to initialize GLEW\n");
+
+    glfwTerminate();
     return -1;
   }
 
@@ -108,6 +156,34 @@ int main(int argc, char** argv) {
 
   // Dark blue background
   glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+  // Some enablement
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
+
+  // Initialize vertexarray
+  GLuint vertex_array_id;
+  glGenVertexArrays(1, &vertex_array_id);
+  glBindVertexArray(vertex_array_id);
+
+  // Loading shaders into program
+  GLuint program_id = load_shaders(default_shader_vertex, default_shader_fragment);
+
+  // printf("program_id: %i", program_id);
+  
+  // Getting ID for uniforms
+  GLuint mvp_matrix_id = glGetUniformLocation(program_id, "MVP");
+
+  // CAMERA STUFF - PROJECTION MATRICIES
+  // @TODO - move FOV and RATIO to different place, you know which is which, also near and far as inside parameters of game
+  glm::mat4 projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1, 100.0f);
+  glm::mat4 view = glm::lookAt(
+      glm::vec3(4, 3, -3), // Camera pos
+      glm::vec3(0, 0, 0),  // Where to look at
+      glm::vec3(0, 1, 0)   // Camera up vector
+      );
+  glm::mat4 model = glm::mat4(1.0f);
+  glm::mat4 MVP = projection * view * model;
 
   // Main loop
   while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0) {
